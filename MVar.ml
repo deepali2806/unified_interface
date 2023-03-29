ef93db00e702e9206a5ca1f8d46f90d6acbe1af3
Deepali <deepaliande.1@gmail.com>
2023-03-29 19:38:59 +0530

    Remove unnecessary comments and references

--- MVar.ml
+++ MVar.ml
@@ -1,6 +1,5 @@
 open Effect
 
-exception Abort of string
 
 type 'a mv_state =
   | Full  of 'a * ('a * unit Sched.resumer) Fun_queue.t
@@ -16,13 +15,12 @@ let create v = Atomic.make (Full (v, Fun_queue.empty))
 let rec put mv v =
   let old_contents = Atomic.get (mv) in
   match old_contents with
-  | Full (v', q) -> let p = ref true in 
-                    (try perform (Sched.Suspend (fun r -> 
+  | Full (v', q) -> (try perform (Sched.Suspend (fun r -> 
                                             let newQueue = Fun_queue.push q (v,r) in
                                             let new_contents = Full (v', newQueue) in
-                                            p := Atomic.compare_and_set mv old_contents new_contents;
-                                            !p
-                                            )) 
+                                            Atomic.compare_and_set mv old_contents new_contents
+                                          )
+                                 ) 
                     with
                     | Exit -> put mv v)
   | Empty q ->
@@ -41,15 +39,12 @@ let rec put mv v =
                                                   let ret = Atomic.compare_and_set mv old_contents new_contents in 
                                                   if ret then
                                                     begin
-                                                      (* resume (Ok v);
-                                                      () *)
-                                                      (* This was added for cancellation purpose ::==> *)
+                                                      (* This was added for cancellation purpose*)
                                                       let ret1 = resume (v) in
                                                       if ret1 = Resume_success then ()
                                                       else 
                                                       (* Retrying *)
                                                         put mv v
-                                                      (* raise (Abort "Excception in Put because it is already aborted")   *)
                                                     end                                               
                                                   else
                                                     put mv v
@@ -59,17 +54,13 @@ let rec put mv v =
 let rec take mv = 
   let old_contents = Atomic.get mv in 
   match old_contents with
-  | Empty q -> let p = ref true in 
-                  (try perform (Sched.Suspend (fun r -> 
+  | Empty q -> (try perform (Sched.Suspend (fun r -> 
                                             let newQueue = Fun_queue.push q r in
                                             let new_contents = Empty newQueue in
-                                            p := Atomic.compare_and_set mv old_contents new_contents;
-                                            !p 
+                                            Atomic.compare_and_set mv old_contents new_contents
                                           )
-                        )
-                  with Exit -> 
-                  (*Printf.printf "\nInside Exit of take%!";*)
-                                  take mv )
+                            )
+                  with Exit -> take mv )
                              
   | Full (v, q) ->
                 if Fun_queue.length q = 0 then
@@ -83,18 +74,15 @@ let rec take mv =
                   end 
                 else
                     match Fun_queue.pop q with
-                    | None -> raise (Abort "Control will never reach here, Exception in take when popping from empty queue")
+                    | None -> raise Exit
                     | Some ((v', resume), newQueue) -> 
                                                   let new_contents = Full (v', newQueue) in
                                                   let ret = Atomic.compare_and_set mv old_contents new_contents in 
                                                   if ret then
                                                     begin
-                                                      (* resume (Ok ());
-                                                      v *)
                                                       let ret1 = resume ( ()) in 
                                                       if ret1 = Resume_success then v
                                                       else take mv
-                                                      (* raise (Abort "Excception in Take because it is already aborted")    *)
                                                     end
                                                   else
                                                     take mv                 
